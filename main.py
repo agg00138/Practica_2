a5b97b8ecb64ea154630977c7139f957d99ba26d
agg00138 <agg00138@red.ujaen.es>
2024-11-02 15:22:22 +0100

    Update main.py with greedy example

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+# main.py
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Importaciones de bibliotecas estándar
+import sys
+import os
+import random
+
+# Importaciones locales
+from auxiliares.procesador_archivos import ProcesadorTXT, ProcesadorTSP
+from auxiliares.funciones_generales import generar_semillas
+from algoritmos.AlgGRE_Clase01_Grupo06 import GreedyAleatorio
+
+# Importaciones de terceros
+import numpy as np
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    # Muestra un mensaje de bienvenida.
+    print(f'¡Hola {name}! 👋 Esperamos que te guste nuestra práctica. ¡Estamos muy emocionados! 😊🚀💻\n')
 
+def main():
+    # Comprueba que se pasen 2 argumentos por consola
+    if len(sys.argv) != 2:
+        print('Uso: (python | py) ./main.py ./config.txt')
+        sys.exit(1)
 
-# Press the green button in the gutter to run the script.
+    # Almacena el fichero config.txt
+    archivo_config = sys.argv[1]
+    txt = ProcesadorTXT(archivo_config)
+    parametros = txt.cargar_datos()
+
+    print('Parámetros procesados:')
+    for clave, valor in parametros.items():
+        print(f'{clave} = {valor}')
+
+    # Genera las semillas
+    semillas = generar_semillas(parametros['dni'], parametros['num_ejecuciones'])
+
+    print('Semillas generadas:', semillas)
+
+    # Obtiene los archivos .tsp
+    archivos_tsp = parametros['archivos']
+
+    # Procesa un archivo .tsp
+    for archivo_tsp in archivos_tsp:
+        ruta_archivo = os.path.join('data', archivo_tsp)
+        tsp = ProcesadorTSP(ruta_archivo)
+        matriz, tour = tsp.cargar_datos()
+        print('\n======================')
+        print(f'Procesando {archivo_tsp}:')
+        print('======================')
+
+        # Ejecuta el algoritmo greedy aleatorio (comprobación)
+        for i, semilla in enumerate(semillas, start=1):
+            # Configura la semilla para esta ejecución
+            random.seed(semilla)
+            np.random.seed(semilla)
+
+            # Ejecuta el algoritmo Greedy como ejemplo (esto también aplicaría a otros algoritmos)
+            greedy = GreedyAleatorio(matriz, parametros)
+            tour, distancia = greedy.ejecutar()
+            tsp.mostrar_tour(tour)
+            print(f'Distancia: {distancia}\n')
+
+# Presiona el botón de 'play' para ejecutar el script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_hi('Cristobal')
+    main()
