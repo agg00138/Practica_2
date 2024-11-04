9b0ac2db88b7f337a1a82689c0fcd74af660245c
agg00138 <agg00138@red.ujaen.es>
2024-11-05 00:56:26 +0100

    Update individuo.py

--- modelos/individuo.py
+++ modelos/individuo.py
@@ -1,5 +1,8 @@
 # modelos/individuo.py
 
+# Importaciones locales
+from auxiliares.funciones_generales import factorizacion
+
 # Importaciones de terceros
 import numpy as np
 
@@ -16,6 +19,8 @@ class Individuo:
         else:
             self.distancia = distancia
 
+        self.fitness = distancia    # Valor fitness del individuo
+
 
     def calcular_distancia(self, matriz):
         """Calcula la distancia total de un tour."""
@@ -24,4 +29,16 @@ class Individuo:
 
     def mutar(self):
         """Realiza una mutación (2-opt) en el individuo."""
-        pass
+        n = len(self.tour)
+        i, j = np.random.choice(range(1, n - 1), size=2, replace=False)  # Evita el primer y último índice
+
+        # Aplica el operador de intercambio 2-opt
+        mutacion_tour = self.tour.copy()
+        mutacion_tour[i], mutacion_tour[j] = self.tour[j], self.tour[i]
+
+        # Calcula (eficientemente) la distancia total del nuevo tour
+        arcos_desaparecen, arcos_nuevos = factorizacion(self.tour, self.matriz, i, j)
+        nueva_distancia_total = self.distancia - arcos_desaparecen + arcos_nuevos
+
+        self.tour = mutacion_tour
+        self.distancia = self.fitness = nueva_distancia_total
