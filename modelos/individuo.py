bdb3788307729a488035f32057cad346d3963df6
agg00138 <agg00138@red.ujaen.es>
2024-11-16 12:42:16 +0100

    Update individuo.py with def factorizacion

--- modelos/individuo.py
+++ modelos/individuo.py
@@ -1,7 +1,6 @@
 # modelos/individuo.py
 
 # Importaciones locales
-from auxiliares.funciones_generales import factorizacion
 
 # Importaciones de terceros
 import numpy as np
@@ -33,12 +32,33 @@ class Individuo:
         i, j = np.random.choice(range(1, n - 1), size=2, replace=False)  # Evita el primer y último índice
 
         # Aplica el operador de intercambio 2-opt
-        arcos_desaparecen, arcos_nuevos = factorizacion(self.tour, self.matriz, i, j)
+        arcos_desaparecen, arcos_nuevos = self.factorizacion(i, j)
         nueva_distancia_total = self.distancia - arcos_desaparecen + arcos_nuevos
 
         self.tour[i], self.tour[j] = self.tour[j], self.tour[i]     # Actualiza los índices del tour
         self.distancia = nueva_distancia_total
         self.fitness = nueva_distancia_total
+
+
+    def factorizacion(self, i, j):
+        """Calcula eficientemente la distancia de un tour tras un intercambio 2-opt."""
+        n = len(self.tour)
+
+        # Manejar el caso cuando las ciudades son consecutivas
+        if abs(i - j) == 1 or (i == 0 and j == n - 1) or (i == n - 1 and j == 0):
+            arcos_desaparecen = self.matriz[self.tour[i - 1], self.tour[i]] + self.matriz[self.tour[j], self.tour[(j + 1) % n]]
+            arcos_nuevos = self.matriz[self.tour[i - 1], self.tour[j]] + self.matriz[self.tour[i], self.tour[(j + 1) % n]]
+        else:
+            arcos_desaparecen = (
+                    self.matriz[self.tour[i - 1], self.tour[i]] + self.matriz[self.tour[i], self.tour[(i + 1) % n]] +
+                    self.matriz[self.tour[j - 1], self.tour[j]] + self.matriz[self.tour[j], self.tour[(j + 1) % n]]
+            )
+            arcos_nuevos = (
+                    self.matriz[self.tour[i - 1], self.tour[j]] + self.matriz[self.tour[j], self.tour[(i + 1) % n]] +
+                    self.matriz[self.tour[j - 1], self.tour[i]] + self.matriz[self.tour[i], self.tour[(j + 1) % n]]
+            )
+
+        return arcos_desaparecen, arcos_nuevos
 
 
     def __repr__(self):
